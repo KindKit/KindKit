fcc7e2d0cd97c63a67e173647788a1f5d54d95c4
Alexander Trifonov <fgengine@gmail.com>
2022-02-21 13:30:09 +0300

    Fix RawRepresentableJsonCoder & EnumJsonCoder

--- Sources/Json/Coder/Swift/EnumJsonCoder.swift
+++ Sources/Json/Coder/Swift/EnumJsonCoder.swift
@@ -5,21 +5,21 @@
 import Foundation
 import KindKitCore
 
-public struct EnumJsonDecoder< Enum : IEnumDecodable > : IJsonValueDecoder where Enum.RawValue : IJsonDecoderAlias, Enum.RawValue == Enum.RawValue.JsonDecoder.Value {
+public struct EnumJsonDecoder< Enum : IEnumDecodable, Decoder: IJsonValueDecoder > : IJsonValueDecoder where Enum.RawValue == Decoder.Value {
     
     public static func decode(_ value: IJsonValue) throws -> Enum.RealValue {
-        guard let rawValue = try? Enum.RawValue.JsonDecoder.decode(value) else { throw JsonError.cast }
+        guard let rawValue = try? Decoder.decode(value) else { throw JsonError.cast }
         guard let decoded = Enum(rawValue: rawValue) else { throw JsonError.cast }
         return decoded.realValue
     }
     
 }
 
-public struct EnumJsonEncoder< Enum : IEnumEncodable > : IJsonValueEncoder where Enum.RawValue : IJsonEncoderAlias, Enum.RawValue == Enum.RawValue.JsonEncoder.Value {
+public struct EnumJsonEncoder< Enum : IEnumEncodable, Encoder: IJsonValueEncoder > : IJsonValueEncoder where Enum.RawValue == Encoder.Value {
     
     public static func encode(_ value: Enum.RealValue) throws -> IJsonValue {
         let encoded = Enum(realValue: value)
-        return try Enum.RawValue.JsonEncoder.encode(encoded.rawValue)
+        return try Encoder.encode(encoded.rawValue)
     }
     
 }
