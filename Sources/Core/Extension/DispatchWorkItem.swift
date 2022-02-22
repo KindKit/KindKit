aa185529a6b3eaecc0db230591ffc111cc036d44
Alexander Trifonov <fgengine@gmail.com>
2022-02-22 09:29:48 +0300

    Extend async functions on DispatchWorkItem

--- Sources/Core/Extension/DispatchWorkItem.swift
+++ Sources/Core/Extension/DispatchWorkItem.swift
@@ -4,5 +4,21 @@
 
 import Foundation
 
+public extension DispatchWorkItem {
+    
+    static func async(block: @escaping () -> Void, queue: DispatchQueue) -> DispatchWorkItem {
+        let workItem = DispatchWorkItem(block: block)
+        queue.async(execute: workItem)
+        return workItem
+    }
+    
+    static func async(block: @escaping () -> Void, queue: DispatchQueue, after: DispatchTime) -> DispatchWorkItem {
+        let workItem = DispatchWorkItem(block: block)
+        queue.asyncAfter(deadline: after, execute: workItem)
+        return workItem
+    }
+    
+}
+
 extension DispatchWorkItem : ICancellable {
 }
