e607077d0e71b6b8079c3b8ce2044def883fc957
Alexander Trifonov <fgengine@gmail.com>
2023-06-28 16:54:44 +0300

    Improvement database 3

--- Sources/KindKit/Database/Database+ValueCoder+Date+UnixTime.swift
+++ Sources/KindKit/Database/Database+ValueCoder+Date+UnixTime.swift
@@ -6,7 +6,7 @@ import Foundation
 
 public extension Database.ValueCoder.Date {
     
-    struct UnitTime : IDatabaseValueCoder {
+    struct UnixTime : IDatabaseValueCoder {
         
         public typealias DatabaseDecoded = Foundation.Date
         
